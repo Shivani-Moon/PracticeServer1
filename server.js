07ec45ec31012c9cf3fe7697e1c30bea4ffb3030
Shivani-Moon <shivani.it.2023@gmail.com>
2025-09-05 10:11:02 +0530

    fs(file system)

--- server.js
+++ server.js
@@ -43,7 +43,51 @@ const filePath = path.join(dirPath, 'example.txt');
 fs.mkdir(dirPath, { recursive: true }, (err) => {
     if (err) throw err;
     console.log('Directory created.');
-})
+
+
+// 2. Write to File
+
+fs.writeFile(filePath, 'Hello, this is an exampke text!', (err) =>{
+    if (err) throw err;
+    console.log('File Written.');
+
+
+    // 3. Read File
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) throw err;
+        console.log('File content:', data);
+
+        //4. Append to File
+        fs.appendFile(filePath, '\nAppending more content.' , (err) => {
+            if (err) throw err;
+            console.log('Content appended.');
+
+            //5. Rename File
+            const newFilePath = path.join(dirPath, 'renamedExample.txt');
+            fs.rename(filePath, newFilePath, (err) => {
+                if (err) throw err;
+                console.log('File renamed.');
+
+                //6. Delete File
+                fs.unlink(newFilePath, (err) => {
+                    if (err) throw err;
+                    console.log('File Deleted');
+
+
+                    //7. Delete Directory
+
+                    fs.rm(dirPath, { recursive: true, force: true }, (err) => {
+                        if (err) throw err;
+                        console.log('Directory deleted.');
+                    });
+                });
+            });
+        });
+    });
+});
+});
+
+
 
 
 
